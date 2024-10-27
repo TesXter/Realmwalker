cd6b044e0c3fa2a4ebc504cef710ae083934a7a2
TesXter <138005668+TesXter@users.noreply.github.com>
2024-10-27 12:05:27 +0700

    just change gui

--- gui.lua
+++ gui.lua
@@ -1,8 +1,11 @@
 local gui = {}
-local developer_id = "TesXter_Realmwalker"
+local developer_id = "TesXter"
+local script_name = "Realmwalker"
+local code_name = get_hash(developer_id .. "_" .. script_name)
+local version = "0.3"
 
 local function create_checkbox(key)
-    return checkbox:new(false, get_hash(developer_id .. "_" .. key))
+    return checkbox:new(false, get_hash(code_name .. "_" .. key))
 end
 
 gui.elements = {
@@ -11,7 +14,7 @@ gui.elements = {
 }
 
 function gui.render()
-    if not gui.elements.main_tree:push("Realmwalker 0.3") then return end
+    if not gui.elements.main_tree:push("Realmwalker | " .. developer_id .. " | V" .. version) then return end
 
     gui.elements.main_toggle:render("Enable", "")
     if not gui.elements.main_toggle:get() then
